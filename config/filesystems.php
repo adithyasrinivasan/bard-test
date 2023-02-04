1152e55a8397dbfee72a29f3064ddb3398146770
Adithya <sharks12@live.in>
2023-02-04 11:36:11 +0100

    User and asset driver

--- config/filesystems.php
+++ config/filesystems.php
@@ -74,6 +74,13 @@ return [
             'visibility' => 'public',
         ],
 
+        'assets' => [
+            'driver' => 'local',
+            'root' => public_path('assets'),
+            'url' => '/assets',
+            'visibility' => 'public',
+        ],
+
         'favicons' => [
             'driver' => 'local',
             'root' => public_path('favicons'),

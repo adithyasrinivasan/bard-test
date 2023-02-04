522170720d6ca623d93012dbd145366db352f694
Adithya <sharks12@live.in>
2023-02-04 11:47:30 +0100

    Setup bard mutator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\View;
 use Statamic\Statamic;
 use Statamic\Facades\GlobalSet;
 use Statamic\Fieldtypes\Section;
+use JackSleight\StatamicBardMutator\Facades\Mutator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,7 +18,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            \Tiptap\Editor::class,
+            \JackSleight\StatamicBardMutator\Editor::class
+        );
     }
 
     /**
@@ -39,6 +43,38 @@ class AppServiceProvider extends ServiceProvider
                     $entry = GlobalSet::find('configuration')->inDefaultSite()->error_404_entry;
                 }
                 $view->with($entry->toAugmentedArray());
+            }
+        });
+
+        Mutator::data('paragraph', function ($data, $meta){
+            foreach ($data->content as $i => $node) {
+               
+                if ($node->type !== 'text') {
+                    continue;
+                }
+
+                foreach ($node->marks ?? [] as $mark){
+                    if ($mark->type !== 'italic' && $mark->type !== 'bold' && $mark->type !== 'underline'){
+                        continue;
+                    }
+                }
+
+                $regex = '/^(.*?)\bLorem ipsum\b(.*)$/';
+
+                if (!preg_match($regex, $node->text, $match)) {
+                    continue;
+                }
+                
+                [, $before, $after] = $match;
+                array_splice($data->content, $i, 1, [
+                    (object)['type' => 'text', 'text' => $before],
+                    (object)['type' => 'text', 'text' => 'DUMMY TEXT', 'marks' => [
+                        (object)['type' => 'link', 'attrs' => (object)['href' => 'https://google.com']],
+                    ]],
+                    (object)['type' => 'text', 'text' => $after],
+                ]);
+
+                return;
             }
         });
     }
